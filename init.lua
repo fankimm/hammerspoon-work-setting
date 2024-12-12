afcbbff296ca48683e0d49fa5c2789726301c8ed
jihwan-kim3 <jihwan_kim3@kolon.com>
2024-12-12 11:10:02 +0900

    first commit

---
+++ init.lua
@@ -0,0 +1,182 @@
+-- Hammerspoon configuration to launch Visual Studio Code with Option+1
+
+-- Bind the hotkey
+hs.hotkey.bind({ "alt" }, "1", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bind({ "alt" }, "2", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({ "alt" }, "3", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Terminal")
+end)
+
+hs.hotkey.bind({ "alt" }, "c", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Workplace Chat")
+end)
+
+hs.hotkey.bind({ "alt" }, "m", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Mail")
+end)
+
+hs.hotkey.bind({ "alt" }, "n", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Notes")
+end)
+
+hs.hotkey.bind({ "alt" }, "r", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Microsoft Remote Desktop")
+end)
+
+hs.hotkey.bind({ "alt" }, "j", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("RunJS")
+end)
+
+hs.hotkey.bind({ "alt" }, "e", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Numbers")
+end)
+
+hs.hotkey.bind({ "alt" }, "d", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("DBeaver")
+end)
+
+hs.hotkey.bind({ "alt" }, ",", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("System Settings")
+end)
+
+hs.hotkey.bind({ "alt" }, "`", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Finder")
+end)
+
+hs.hotkey.bind({ "alt" }, "v", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("vpn on")
+end)
+
+hs.hotkey.bind({ "alt", "cmd" }, "k", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("KakaoTalk")
+end)
+
+hs.hotkey.bind({ "alt" }, "a", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Asana")
+end)
+
+hs.hotkey.bind({ "alt" }, "g", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Discord")
+end)
+
+hs.hotkey.bind({ "alt" }, "q", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Calendar")
+end)
+
+hs.hotkey.bind({ "alt" }, "s", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Safari")
+end)
+
+hs.hotkey.bind({ "alt" }, "i", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("IntelliJ IDEA")
+end)
+
+hs.hotkey.bind({ "alt" }, "l", function()
+    -- Launch or focus Visual Studio Code
+    hs.application.launchOrFocus("Slack")
+end)
+
+
+-- 2024-12-12 10:50:08: loginwindow
+-- 2024-12-12 10:50:08: universalaccessd
+-- 2024-12-12 10:50:08: 제어 센터
+-- 2024-12-12 10:50:08: CoreServicesUIAgent
+-- 2024-12-12 10:50:08: WindowManager
+-- 2024-12-12 10:50:08: CoreLocationAgent
+-- 2024-12-12 10:50:08: 알림 센터
+-- 2024-12-12 10:50:08: Wi-Fi
+-- 2024-12-12 10:50:08: chronod
+-- 2024-12-12 10:50:08: UIKitSystem
+-- 2024-12-12 10:50:08: ViewBridgeAuxiliary
+-- 2024-12-12 10:50:08: 배경화면
+-- 2024-12-12 10:50:08: CursorUIViewService
+-- 2024-12-12 10:50:08: 터미널
+-- 2024-12-12 10:50:08: 메모
+-- 2024-12-12 10:50:08: Dock
+-- 2024-12-12 10:50:08: SystemUIServer
+-- 2024-12-12 10:50:08: Spotlight
+-- 2024-12-12 10:50:08: Finder
+-- 2024-12-12 10:50:08: QuickLookUIService(Finder)
+-- 2024-12-12 10:50:08: 추가 Dock
+-- 2024-12-12 10:50:08: Dock Extra (Proxyman.app)
+-- 2024-12-12 10:50:08: Dock Extra (Arc.app)
+-- 2024-12-12 10:50:08: com.apple.CoreSimulator.CoreSimulatorService
+-- 2024-12-12 10:50:08: Simulator
+-- 2024-12-12 10:50:08: Clipy
+-- 2024-12-12 10:50:08: TextInputMenuAgent
+-- 2024-12-12 10:50:08: AirPlay 화면 미러링
+-- 2024-12-12 10:50:08: Siri
+-- 2024-12-12 10:50:08: Workplace ChatHelper
+-- 2024-12-12 10:50:08: JetBrains Toolbox
+-- 2024-12-12 10:50:08: HotKey
+-- 2024-12-12 10:50:08: Logi Options+
+-- 2024-12-12 10:50:08: ESET Endpoint Security
+-- 2024-12-12 10:50:08: delfino
+-- 2024-12-12 10:50:08: SiriNCService
+-- 2024-12-12 10:50:08: CalendarWidgetExtension
+-- 2024-12-12 10:50:08: RemindersIntentsExtension
+-- 2024-12-12 10:50:08: ShortcutsWidgetExtension
+-- 2024-12-12 10:50:08: macOSWidgetExtension
+-- 2024-12-12 10:50:08: com.apple.PressAndHold
+-- 2024-12-12 10:50:08: 한국어 IM
+-- 2024-12-12 10:50:08: PowerChime
+-- 2024-12-12 10:50:08: Asana
+-- 2024-12-12 10:50:08: Code
+-- 2024-12-12 10:50:08: Google Chrome
+-- 2024-12-12 10:50:08: Google Chrome Helper (Plugin)
+-- 2024-12-12 10:50:08: Safari
+-- 2024-12-12 10:50:08: Safari Networking
+-- 2024-12-12 10:50:08: Safari Graphics and Media
+-- 2024-12-12 10:50:08: com.apple.Safari.SandboxBroker(Safari)
+-- 2024-12-12 10:50:08: Open and Save Panel Service(Safari)
+-- 2024-12-12 10:50:08: QuickLookUIService(Open and Save Panel Service(Safari))
+-- 2024-12-12 10:50:08: Safari 웹 콘텐츠(캐시됨)
+-- 2024-12-12 10:50:08: LinkedNotesUIService
+-- 2024-12-12 10:50:08: Keychain Circle Notification
+-- 2024-12-12 10:50:08: 카카오톡
+-- 2024-12-12 10:50:08: 암호 자동 완성(카카오톡)
+-- 2024-12-12 10:50:08: TextInputSwitcher
+-- 2024-12-12 10:50:08: coreautha
+-- 2024-12-12 10:50:08: AuthenticationServicesAgent
+-- 2024-12-12 10:50:08: Safari 웹 콘텐츠
+-- 2024-12-12 10:50:08: SoftwareUpdateNotificationManager
+-- 2024-12-12 10:50:08: BackgroundTaskManagementAgent
+-- 2024-12-12 10:50:08: npm exec create-medusa-app@latest
+-- 2024-12-12 10:50:08: npm run dev
+-- 2024-12-12 10:50:08: next-server (v14.2.15)
+-- 2024-12-12 10:50:08: Workplace Chat
+-- 2024-12-12 10:50:08: Single Sign-On
+-- 2024-12-12 10:50:08: Logi AI Prompt Builder
+-- 2024-12-12 10:50:08: nsattributedstringagent
+-- 2024-12-12 10:50:08: nbagent
+-- 2024-12-12 10:50:08: OSDUIHelper
+-- 2024-12-12 10:50:08: Hammerspoon
+-- 2024-12-12 10:50:08: universalAccessAuthWarn
+-- 2024-12-12 10:50:08: Safari 웹 콘텐츠
+-- 2024-12-12 10:50:08: Open and Save Panel Service(Code)
+-- 2024-12-12 10:50:08: QuickLookUIService(Open and Save Panel Service(Code))
