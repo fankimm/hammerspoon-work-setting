67243e916d75bc43bed01bf9bba510e3701e9889
jihwan-kim3 <jihwan_kim3@kolon.com>
2025-01-22 11:07:05 +0900

    불필요 주석 제거

--- init.lua
+++ init.lua
@@ -1,102 +1,79 @@
--- Hammerspoon configuration to launch Visual Studio Code with Option+1
-
--- Bind the hotkey
 hs.hotkey.bind({ "alt" }, "1", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Visual Studio Code")
 end)
 
 hs.hotkey.bind({ "alt" }, "2", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Google Chrome")
 end)
 
 hs.hotkey.bind({ "alt" }, "3", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Terminal")
 end)
 
 hs.hotkey.bind({ "alt" }, "c", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Workplace Chat")
 end)
 
 hs.hotkey.bind({ "alt" }, "m", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Mail")
 end)
 
 hs.hotkey.bind({ "alt" }, "n", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Notes")
 end)
 
 hs.hotkey.bind({ "alt" }, "r", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Microsoft Remote Desktop")
 end)
 
 hs.hotkey.bind({ "alt" }, "j", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("RunJS")
 end)
 
 hs.hotkey.bind({ "alt" }, "e", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Numbers")
 end)
 
 hs.hotkey.bind({ "alt" }, "d", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("DBeaver")
 end)
 
 hs.hotkey.bind({ "alt" }, ",", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("System Settings")
 end)
 
 hs.hotkey.bind({ "alt" }, "`", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Finder")
 end)
 
 hs.hotkey.bind({ "alt" }, "v", function()
-    -- Launch or focus Visual Studio Code
-    hs.application.launchOrFocus("vpn on")
+    hs.application.launchOrFocus("vpn_toggle")
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "k", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("KakaoTalk")
 end)
 
 hs.hotkey.bind({ "alt" }, "a", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Asana")
 end)
 
 hs.hotkey.bind({ "alt" }, "g", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Discord")
 end)
 
 hs.hotkey.bind({ "alt" }, "q", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Calendar")
 end)
 
 hs.hotkey.bind({ "alt" }, "s", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Safari")
 end)
 
 hs.hotkey.bind({ "alt" }, "i", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("IntelliJ IDEA")
 end)
 
 hs.hotkey.bind({ "alt" }, "l", function()
-    -- Launch or focus Visual Studio Code
     hs.application.launchOrFocus("Slack")
 end)
